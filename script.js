047eb33ed8d22ad2fe625507cd9355299f6ef732
sogoln81-jpg <sogoln81@gmail.com>
2026-07-01 16:08:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const s=new Date('2020-07-29T22:40:00');function u(){let d=Math.floor((Date.now()-s)/86400000);counter.textContent='Together for '+d+' days ❤️';}u();setInterval(u,60000);
+const start = new Date("2020-07-29T22:40:00");
+
+function updateCounter() {
+
+const now = new Date();
+
+const diff = now - start;
+
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+
+const minutes = Math.floor((diff / (1000 * 60)) % 60);
+
+const seconds = Math.floor((diff / 1000) % 60);
+
+document.getElementById("counter").innerHTML =
+`❤️ Together for<br><br>
+<b>${days}</b> Days
+<b>${hours}</b> Hours
+<b>${minutes}</b> Minutes
+<b>${seconds}</b> Seconds`;
+
+}
+
+setInterval(updateCounter,1000);
+
+updateCounter();
